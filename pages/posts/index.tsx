90fd9e611d95e864658986907683bfda5ed8e8c1
sosukesuzuki <aosukeke@gmail.com>
2021-02-24 16:03:04 +0900

    Show all posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,29 @@
+import { GetStaticProps } from "next";
+import Link from "next/link";
 import Layout from "../../src/components/Layout";
+import { getAllPosts, PostData } from "../../src/utils/posts";
 
-const Posts = () => <Layout title="記事一覧">やほ</Layout>;
+type Props = {
+  posts: PostData[];
+};
+
+const Posts = ({ posts }: Props) => (
+  <Layout title="記事一覧">
+    <div>
+      {posts.map((post) => (
+        <div key={post.slug}>
+          <Link href={`/posts/${post.slug}`}>{post.metaData.title}</Link>
+        </div>
+      ))}
+    </div>
+  </Layout>
+);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPosts();
+  return {
+    props: { posts },
+  };
+};
 
 export default Posts;
