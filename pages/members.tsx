b689586fb44f0457840f62c2636e67fcec048c1a
sosukesuzuki <aosukeke@gmail.com>
2021-02-24 11:01:16 +0900

    Add member icons

--- pages/members.tsx
+++ pages/members.tsx
@@ -1,26 +1,31 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 
 type Member = {
   name: string;
+  iconUrl: string;
   twitterId?: string;
   githubUsername?: string;
 };
 
 const members: Member[] = [
   {
-    name: "sosukesuzuki",
-    twitterId: "__sosukesuzuki",
-    githubUsername: "sosukesuzuki",
+    name: "sakito",
+    iconUrl: "/member-icons/sakito.jpg",
+    twitterId: "__sakito__",
+    githubUsername: "sakito21",
   },
   {
     name: "BaHo",
+    iconUrl: "/member-icons/baho.jpg",
     twitterId: "b4h0_c4t",
     githubUsername: "b4h0-c4t",
   },
   {
-    name: "sakito",
-    twitterId: "__sakito__",
-    githubUsername: "sakito21",
+    name: "sosukesuzuki",
+    iconUrl: "/member-icons/sosukesuzuki.jpg",
+    twitterId: "__sosukesuzuki",
+    githubUsername: "sosukesuzuki",
   },
 ];
 
@@ -28,6 +33,7 @@ const Member = ({ member }: { member: Member }) => {
   return (
     <section>
       <p>{member.name}</p>
+      <Image src={member.iconUrl} width="100" height="100" />
       <a
         href={`https://twitter.com/${member.twitterId}`}
       >{`@${member.twitterId}`}</a>
