2d748a26d7149c1b81c4c0a50665a98865bef9bb
sosukesuzuki <aosukeke@gmail.com>
2021-02-24 11:08:38 +0900

    Remove link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import Layout from "../components/Layout";
-import Link from "next/link";
 
 const IndexPage = () => (
   <Layout>
     <h1>Hello Cybozu Frontend Expert Team 👋</h1>
-    <Link href="/members">メンバー</Link>
   </Layout>
 );
 
