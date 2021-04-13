46e6111f0dfc03df7c96ac89cd494c6eb0277a08
sosukesuzuki <aosukeke@gmail.com>
2021-04-13 11:54:49 +0900

    Add all members

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -4,27 +4,48 @@ import { SpeakerDeck } from "../components/SpeakerDeck";
 
 type Member = {
   name: string;
-  iconUrl: string;
   twitterId?: string;
   githubUsername?: string;
 };
 
 const members: Member[] = [
   {
+    name: "koba04",
+    twitterId: "koba04",
+    githubUsername: "koba04",
+  },
+  {
+    name: "pirosikick",
+    twitterId: "pirosikick",
+    githubUsername: "pirosikick",
+  },
+  {
+    name: "zaki___yama",
+    twitterId: "zaki___yama",
+    githubUsername: "zaki-yama",
+  },
+  {
     name: "sakito",
-    iconUrl: "/member-icons/sakito.jpg",
     twitterId: "__sakito__",
     githubUsername: "sakito21",
   },
   {
+    name: "shisama",
+    twitterId: "shisama_",
+    githubUsername: "shisama",
+  },
+  {
+    name: "nakajmg",
+    twitterId: "nakajmg",
+    githubUsername: "nakajmg",
+  },
+  {
     name: "BaHo",
-    iconUrl: "/member-icons/baho.jpg",
     twitterId: "b4h0_c4t",
     githubUsername: "b4h0-c4t",
   },
   {
     name: "sosukesuzuki",
-    iconUrl: "/member-icons/sosukesuzuki.jpg",
     twitterId: "__sosukesuzuki",
     githubUsername: "sosukesuzuki",
   },
@@ -34,7 +55,12 @@ const Member = ({ member }: { member: Member }) => {
   return (
     <section>
       <p>{member.name}</p>
-      <Image src={member.iconUrl} width="100" height="100" />
+      <Image
+        src={`/member-icons/${member.name}.jpg`}
+        alt={`${member.name} icon`}
+        width="100"
+        height="100"
+      />
       <a
         href={`https://twitter.com/${member.twitterId}`}
       >{`@${member.twitterId}`}</a>
