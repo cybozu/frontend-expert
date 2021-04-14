4056256cc9810015797c8d10de7b6aad9e1e18f2
sosukesuzuki <aosukeke@gmail.com>
2021-04-15 07:29:54 +0900

    Extract members

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -1,55 +1,7 @@
 import Image from "next/image";
 import { Layout } from "../components/Layout";
 import styles from "./members.module.css";
-
-type Member = {
-  name: string;
-  twitterId?: string;
-  githubUsername?: string;
-};
-
-const members: Member[] = [
-  {
-    name: "koba04",
-    twitterId: "koba04",
-    githubUsername: "koba04",
-  },
-  {
-    name: "pirosikick",
-    twitterId: "pirosikick",
-    githubUsername: "pirosikick",
-  },
-  {
-    name: "zaki___yama",
-    twitterId: "zaki___yama",
-    githubUsername: "zaki-yama",
-  },
-  {
-    name: "sakito",
-    twitterId: "__sakito__",
-    githubUsername: "sakito21",
-  },
-  {
-    name: "shisama",
-    twitterId: "shisama_",
-    githubUsername: "shisama",
-  },
-  {
-    name: "nakajmg",
-    twitterId: "nakajmg",
-    githubUsername: "nakajmg",
-  },
-  {
-    name: "BaHo",
-    twitterId: "b4h0_c4t",
-    githubUsername: "b4h0-c4t",
-  },
-  {
-    name: "sosukesuzuki",
-    twitterId: "__sosukesuzuki",
-    githubUsername: "sosukesuzuki",
-  },
-];
+import { Member, members } from "../utils/members";
 
 const Member = ({ member }: { member: Member }) => {
   return (
---
+++ src/utils/members.ts
@@ -0,0 +1,48 @@
+export type Member = {
+  name: string;
+  twitterId?: string;
+  githubUsername?: string;
+};
+
+export const members: Member[] = [
+  {
+    name: "koba04",
+    twitterId: "koba04",
+    githubUsername: "koba04",
+  },
+  {
+    name: "pirosikick",
+    twitterId: "pirosikick",
+    githubUsername: "pirosikick",
+  },
+  {
+    name: "zaki___yama",
+    twitterId: "zaki___yama",
+    githubUsername: "zaki-yama",
+  },
+  {
+    name: "sakito",
+    twitterId: "__sakito__",
+    githubUsername: "sakito21",
+  },
+  {
+    name: "shisama",
+    twitterId: "shisama_",
+    githubUsername: "shisama",
+  },
+  {
+    name: "nakajmg",
+    twitterId: "nakajmg",
+    githubUsername: "nakajmg",
+  },
+  {
+    name: "BaHo",
+    twitterId: "b4h0_c4t",
+    githubUsername: "b4h0-c4t",
+  },
+  {
+    name: "sosukesuzuki",
+    twitterId: "__sosukesuzuki",
+    githubUsername: "sosukesuzuki",
+  },
+];
