f5572d5667261435e4eef4692d3156e8f7b6ac19
sosukesuzuki <aosukeke@gmail.com>
2021-08-24 14:31:45 +0900

    Update nextjs to 11

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  extends: "@cybozu/eslint-config/presets/react-typescript-prettier",
+  extends: [
+    "@cybozu/eslint-config/presets/react-typescript-prettier",
+    "next",
+    "plugin:@next/next/recommended"
+  ],
   rules: {
+    "spaced-comment": "off",
     "react/react-in-jsx-scope": "off",
     "jsx-a11y/anchor-is-valid": [
       "error",
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -6,8 +6,9 @@ export const Tags = ({ tags }: { tags: string[] }) => {
     <div className={styles.tags}>
       {tags.map((tag) => {
         return (
-          <Link key={tag} href={`/tags/${tag}`}>
-            <div className={styles.tag}>{tag}</div>
+          <Link key={tag} href={`/tags/${tag}`} passHref>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid -- passHref で渡しているから */}
+            <a className={styles.tag}>{tag}</a>
           </Link>
         );
       })}
