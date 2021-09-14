38539e8ccc6a116cbf273e314eb83b376d8993ff
sosukesuzuki <aosukeke@gmail.com>
2021-09-14 16:08:03 +0900

    Suppor multiple entries

---
+++ data/posts/second-post.md
@@ -0,0 +1,9 @@
+---
+title: "サイボウズフロントエンドエキスパートチームのウェブサイトを作りましたよ！！！！"
+author: "sosukesuzuki"
+createdAt: "2021-09-14"
+tags:
+  - Announcement
+---
+
+こんにちは、サイボウズフロントエンドエキスパートチームの[@__sosukesuzuki](https://twitter.com/__sosukesuzuki)です。
--- scripts/og-image.ts
+++ scripts/og-image.ts
@@ -17,19 +17,20 @@ captureOgImages()
 async function captureOgImages() {
   const browser = await puppeteer.launch();
   const posts = await getPosts();
-  const page = await browser.newPage();
-  await page.setViewport({
-    width: 1200,
-    height: 630,
-  });
-  await page.goto("file://" + OG_SOURCE_HTML_FILE_PATH);
   for (const { title, slug } of posts) {
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1200,
+      height: 630,
+    });
+    await page.goto("file://" + OG_SOURCE_HTML_FILE_PATH);
     await page.exposeFunction("getTitle", () => title);
     await page.reload();
     await page.screenshot({
       path: `${OG_DIR_PATH}/` + slug + ".jpg",
       type: "jpeg",
     });
+    await page.close();
   }
   await browser.close();
 }
