554f9357dce627f7cf48fcff4806366400abc37d
sosukesuzuki <aosukeke@gmail.com>
2021-10-26 10:50:19 +0900

    Add script to head

--- src/components/TweetButton.tsx
+++ src/components/TweetButton.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import Head from "next/head";
 import styles from "./TweetButton.module.css";
 
 declare global {
@@ -13,16 +14,20 @@ export const TweetButton = () => {
   }, []);
 
   return (
-    <p className={styles.tweetButton}>
-      <a
-        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-        className="twitter-share-button"
-        data-show-count="false"
-        data-lang="ja"
-      >
-        Tweet
-      </a>
-      <script src="https://platform.twitter.com/widgets.js" async />
-    </p>
+    <>
+      <Head>
+        <script src="https://platform.twitter.com/widgets.js" async />
+      </Head>
+      <p className={styles.tweetButton}>
+        <a
+          href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+          className="twitter-share-button"
+          data-show-count="false"
+          data-lang="ja"
+        >
+          Tweet
+        </a>
+      </p>
+    </>
   );
 };
