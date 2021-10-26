503251febdce5732999a33c9e47965adf7c3b1bb
sosukesuzuki <aosukeke@gmail.com>
2021-10-26 11:12:48 +0900

    Fix

--- src/components/TweetButton.tsx
+++ src/components/TweetButton.tsx
@@ -1,23 +1,18 @@
 import { useEffect } from "react";
-import Head from "next/head";
 import styles from "./TweetButton.module.css";
-
-declare global {
-  interface Window {
-    twttr: any;
-  }
-}
 
 export const TweetButton = () => {
   useEffect(() => {
-    window!.twttr!.widgets!.load();
+    const script = document.createElement("script");
+    script.src = "https://platform.twitter.com/widgets.js";
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
     <>
-      <Head>
-        <script src="https://platform.twitter.com/widgets.js" async />
-      </Head>
       <p className={styles.tweetButton}>
         <a
           href="https://twitter.com/share?ref_src=twsrc%5Etfw"
