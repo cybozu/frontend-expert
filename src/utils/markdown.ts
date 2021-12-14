d07b98fb2930aeb226d0d6ab8746991500ff8605
nakajmg <nakajima.jmg@gmail.com>
2021-12-14 17:13:31 +0900

    apply plugin

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -2,10 +2,12 @@ import remark from "remark";
 import html from "remark-html";
 import prism from "remark-prism";
 import gfm from "remark-gfm";
+import { hatenaLinkCard } from "./hatenaLinkCard";
 
 export async function markdownToHtml(markdownContent: string): Promise<string> {
   const result = await remark()
     .use(gfm)
+    .use(hatenaLinkCard)
     .use(html)
     .use(prism)
     .process(markdownContent);
