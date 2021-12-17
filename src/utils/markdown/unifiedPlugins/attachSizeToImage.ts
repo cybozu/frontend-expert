fcb953c55bd534725e175a9332e0d867d9eaad96
sosukesuzuki <aosukeke@gmail.com>
2021-12-18 02:51:06 +0900

    Add fitToMaxWidth

--- src/utils/markdown/unifiedPlugins/attachSizeToImage.ts
+++ src/utils/markdown/unifiedPlugins/attachSizeToImage.ts
@@ -7,6 +7,25 @@ import sizeOf from "image-size";
 
 const IMAGES_DIR_PATH = path.join(process.cwd(), "public", "image");
 
+type Size = {
+  height: number;
+  width: number;
+};
+
+/**
+ * width が大きすぎたときに、比率を維持して width を小さくする
+ */
+function fitToMaxWidth(size: Size): Size {
+  const widthThreshold = 960 * 0.8;
+  if (size.width < widthThreshold) {
+    return size;
+  }
+  const width = widthThreshold - 50;
+  const ratio = width / size.width;
+  const height = size.height * ratio;
+  return { width, height };
+}
+
 export const attachSizeToImage: Plugin = () => {
   return (tree: Node) => {
     visit(tree, "image", (node: Image, index: number, parent: Parent): void => {
@@ -18,10 +37,14 @@ export const attachSizeToImage: Plugin = () => {
         node.url.replace("/frontend-expert/image/", "")
       );
       try {
-        const { height, width } = sizeOf(imageFilePath);
-        if (height == null || width == null) {
+        const size = sizeOf(imageFilePath);
+        if (size.height == null || size.width == null) {
           return;
         }
+        const { width, height } = fitToMaxWidth({
+          width: size.width,
+          height: size.height,
+        });
         const newNode = {
           type: "html",
           value: `<img src="${node.url}" alt="${node.alt}" width="${width}" height="${height}" >`,
