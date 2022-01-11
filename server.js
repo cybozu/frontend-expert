91683462a87a09b56b532f8d2c86583c1ad8e0f8
sosukesuzuki <aosukeke@gmail.com>
2022-01-12 08:15:45 +0900

    Initial implementation

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require("http");
+const events = require("events");
+const watcher = require("@parcel/watcher");
+const path = require("path");
+
+const postsDirPath = path.join(process.cwd(), "data", "posts");
+
+const emitter = new events.EventEmitter();
+const updatePostsEvent = "update-posts";
+const subscription = watcher.subscribe(postsDirPath, (err, events) => {
+  emitter.emit(updatePostsEvent, events);
+});
+
+function handler(req, res) {
+  const headers = {
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Allow-Credentials": "true",
+  };
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+
+  if (req.url === "/stream") {
+    const emitterHandler = (events) => {
+      for (const event of events) {
+        const sseId = new Date().toLocaleTimeString();
+        res.writeHead(200, {
+          ...headers,
+          "Content-type": "text/event-stream",
+          Connection: "keep-alive",
+        });
+        res.write("id: " + sseId + "\n");
+        res.write("retry: 1000\n");
+        res.write("data: " + "HOHO" + "\n");
+      }
+    };
+    emitter.on(updatePostsEvent, emitterHandler);
+  }
+}
+
+module.exports = function createServer() {
+  return http.createServer(handler);
+};
