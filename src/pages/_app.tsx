fa7041b4f8a6c1f59eb91362714b3a9c9c820237
nakajmg <nakajima.jmg@gmail.com>
2022-04-14 15:21:20 +0900

    add ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from "next/app";
 import "./theme.css";
 import "./app.css";
+import { GoogleAnalytics, usePagesViews } from "nextjs-google-analytics";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  usePagesViews();
+  
+  return <>
+    <GoogleAnalytics/>
+    <Component {...pageProps} />;
+  </>
 };
 
 export default MyApp;
