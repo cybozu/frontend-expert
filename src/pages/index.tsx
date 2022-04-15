4523729c037bb2b96207775b8330605003a093cc
nakajmg <nakajima.jmg@gmail.com>
2022-04-15 10:36:01 +0900

    remove speakerdeck embed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,23 @@
+import { css } from "@emotion/css";
 import { Layout } from "../components/Layout";
 import { PageLayout } from "../components/PageLayout";
-import { SpeakerDeck } from "../components/SpeakerDeck";
 
 const IndexPage = () => (
   <Layout>
     <PageLayout>
       <h2>Hello Cybozu Frontend Expert Team 👋</h2>
-      <SpeakerDeck
-        embedId="0efec8a9dd224baebfb2aaf30fbe9a28"
-        width="100%"
-        title="フロントエンドエキスパートチームについて"
-      />
+      <a href="https://speakerdeck.com/cybozuinsideout/frontendexpert-team" target="_blank">
+        <img className={style} src={"/frontend-expert/speakerdeck-thumbnail.png"} alt="フロントエンドエキスパートチームについて" />
+      </a>
     </PageLayout>
   </Layout>
 );
+
+const style = css`
+  width: 100%;
+  height: auto;
+  border: 1px solid #dbdbdb;
+  border-radius: 3px;
+`
 
 export default IndexPage;
